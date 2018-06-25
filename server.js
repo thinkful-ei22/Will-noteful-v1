6ce6e257cf327123ea352a7be30d5b9019f0f93c
William Holcombe <william.holcombe@uconn.edu>
2018-06-25 15:43:37 -0400

    added enpoint

--- server.js
+++ server.js
@@ -14,6 +14,20 @@ const app = express();
 
 // ADD STATIC SERVER HERE
 
+/*function findNote(body) {
+    // we use destructuring to get the values for adjective1, adjective2, etc.
+    // from the request params
+    const {noteId} = req.params;
+    // then we return a string that substitutes in these values
+    return (
+    
+        data.find(item => item.id === noteId);
+    )};
+
+
+
+*/
+
 app.get('/api/notes', (req, res) => {
     res.json(data);
   });
@@ -21,9 +35,16 @@ app.get('/api/notes', (req, res) => {
     res.json(data[req.params.id]);
   });
 
+  app.get('/api/notes/:id', (req, res) => {
+    const id =req.params.id;
+   const dataNew = data.find(item => item.id === Number(id));
+   return res.json(dataNew);
+
+  });
 
 
 
+ // app.post('/', (req, res) => res.send(findNote(req.noteId)));
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
