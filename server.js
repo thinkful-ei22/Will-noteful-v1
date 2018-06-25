3dd380c8557524f8519ef98fcc57600c837450bd
William Holcombe <william.holcombe@uconn.edu>
2018-06-25 15:09:02 -0400

    added get requests

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ const app = express();
 
 // ADD STATIC SERVER HERE
 
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+  });
+  app.get('/api/notes/:id', (req, res) => {
+    res.json(data[req.params.id]);
+  });
+
+
+
+
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
