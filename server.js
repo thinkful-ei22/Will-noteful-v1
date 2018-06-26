a956775b0def65dcdc58e3f95b83f07355284f8f
William Holcombe <william.holcombe@uconn.edu>
2018-06-26 14:36:44 -0400

    added Express Error-Handling Middleware

--- server.js
+++ server.js
@@ -32,6 +32,21 @@ app.use(requestLogger);
 */
 app.use(express.static('public'));
 
+
+
+
+/*app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+*/
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
 app.get('/api/notes', (req, res) => {
   console.log('Hello');
   const {searchTerm} = req.query;
@@ -53,8 +68,23 @@ app.get('/api/notes/:id', (req, res) => {
 
 });
 
+/*app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+*/
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
 
-
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 // app.post('/', (req, res) => res.send(findNote(req.noteId)));
 
 app.listen(PORT, function () {
